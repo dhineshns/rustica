c373f51faa477db3dd7ea749f2d80beb69c7a45a
Dhinesh Nakkeerar <dhinesh@Dhineshs-MacBook-Pro.local>
2015-03-18 17:32:19 -0400

    inserted inside mongo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/createEvent', function(req, res, next) {
+	inputParams = req.query;
+	var newEventName = inputParams.eventName
+	mongo.connect('mongodb://localhost:27017/rustica', function(err, db){
+		if(err){
+			console.error("err.message")
+		}else{
+			var collection = db.collection('events');
+			collection.insert({
+				eventName : newEventName
+			}, function(err, objectId){
+				res.send({
+				eventId : objectId[0]._id
+				});				
+				console.log("New event inserted!");
+			});
+			
+		}
+	})
+
 });
 
 module.exports = router;
